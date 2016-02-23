33efc9b295b1fe9a85caaaa7b68ae03afd86bc80
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2016-02-23 10:26:29 +0100

    qos is now being respected. must be set before consume.

--- src/Queueing.RabbitMQ/RabbitMQConsumer.cs
+++ src/Queueing.RabbitMQ/RabbitMQConsumer.cs
@@ -28,8 +28,8 @@ namespace Queueing.RabbitMQ
                 _queue = queue;
 
                 _rabbitConsumer = new QueueingBasicConsumer(_model);
-                _model.BasicConsume(queue.Name, false, _rabbitConsumer);
                 _model.BasicQos(0, 1, false);
+                _model.BasicConsume(queue.Name, false, _rabbitConsumer);
             }
             else if (_queue != queue)
             {
