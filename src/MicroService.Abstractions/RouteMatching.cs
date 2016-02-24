cc2c44bc9043afa3b209dd6390c1d38bba4a052d
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2016-02-24 21:46:17 +0100

    Moved routemaching to separate class.

---
+++ src/MicroService.Abstractions/RouteMatching.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroService
+{
+    public class RouteMatching
+    {
+        public static bool RouteMatchesPattern(string route, string pattern)
+        {
+            if (route == pattern)
+            {
+                return true;
+            }
+
+            var routeParts = route.Split('.');
+            var patternParts = pattern.Split('.');
+
+            for (var i = 0; i < patternParts.Length; i++)
+            {
+                var patternPart = patternParts[i];
+
+                if (patternPart == "#")
+                {
+                    return true;
+                }
+                else if (i < routeParts.Length)
+                {
+                    if (routeParts[i] == patternPart || patternPart == "*")
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
